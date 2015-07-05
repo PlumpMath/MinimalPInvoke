c0fd4b14c2dfc23dc7f180eafa2bb2a4b4162179
Jaiden Mispy <jaiden@mispy.me>
2015-07-05 16:41:25 +1000

    Or RESERVED_FUNCTION_POINTERS for now

--- buildemcc+JSIL.bat
+++ buildemcc+JSIL.bat
@@ -1,4 +1,4 @@
 del clib.js
 del /q bin\JSIL\*
-call emcc -s EXPORT_FUNCTION_TABLES=1 -s MODULARIZE=1 -s RESERVED_FUNCTION_POINTERS=8 clib.cpp -o clib.js
+call emcc -s EXPORT_FUNCTION_TABLES=1 -s MODULARIZE=1 clib.cpp -o clib.js
 call ..\JSIL\bin\JSILc MinimalPInvoke.sln --out=bin/JSIL/
